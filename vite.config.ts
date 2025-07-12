2c4a3b3e8a105a7581fde0a00e19a93ff323da8b
vandut <konrad.bielak@gmail.com>
2025-07-12 15:29:29 +0200

    Add PWA screenshots

--- vite.config.ts
+++ vite.config.ts
@@ -3,63 +3,79 @@ import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      plugins: [
-        VitePWA({
-          registerType: 'autoUpdate',
-          devOptions: {
-            enabled: true,
-          },
-          includeAssets: [
-            'icons/favicon.ico',
-            'icons/apple-touch-icon.png',
-            'icons/pwa-192x192.png',
-            'icons/pwa-512x512.png',
-            'icons/pwa-maskable-192x192.png',
-            'icons/pwa-maskable-512x512.png',
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true,
+        },
+        includeAssets: [
+          'icons/favicon.ico',
+          'icons/apple-touch-icon.png',
+          'icons/pwa-192x192.png',
+          'icons/pwa-512x512.png',
+          'icons/pwa-maskable-192x192.png',
+          'icons/pwa-maskable-512x512.png',
+        ],
+        manifest: {
+          name: 'Root Digital Companion',
+          short_name: 'Root Companion',
+          description:
+            "An interactive turn assistant and rules reference for the Root board game. It guides players through setup and gameplay, dynamically providing context-sensitive rules for the active player's faction and turn phase.",
+          theme_color: '#1c1917',
+          background_color: '#D3C6B0',
+          icons: [
+            {
+              src: 'icons/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any',
+            },
+            {
+              src: 'icons/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any',
+            },
+            {
+              src: 'icons/pwa-maskable-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+            {
+              src: 'icons/pwa-maskable-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
           ],
-          manifest: {
-            name: 'Root Digital Companion',
-            short_name: 'Root Companion',
-            description:
-              'An interactive turn assistant and rules reference for the Root board game. It guides players through setup and gameplay, dynamically providing context-sensitive rules for the active player\'s faction and turn phase.',
-            theme_color: '#1c1917',
-            background_color: '#D3C6B0',
-            icons: [
-              {
-                src: 'icons/pwa-192x192.png',
-                sizes: '192x192',
-                type: 'image/png',
-                purpose: 'any',
-              },
-              {
-                src: 'icons/pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'any',
-              },
-              {
-                src: 'icons/pwa-maskable-192x192.png',
-                sizes: '192x192',
-                type: 'image/png',
-                purpose: 'maskable',
-              },
-              {
-                src: 'icons/pwa-maskable-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable',
-              },
-            ],
-          },
-        }),
-      ],
-      base: '/root-digital-companion/',
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+          screenshots: [
+            {
+              src: 'screenshots/mobile-screenshot-1.jpg',
+              sizes: '540x1212',
+              type: 'image/jpeg',
+              form_factor: 'narrow',
+              label: 'Game screen on mobile',
+            },
+            {
+              src: 'screenshots/desktop-screenshot-1.jpg',
+              sizes: '1134x736',
+              type: 'image/jpeg',
+              form_factor: 'wide',
+              label: 'Game screen on desktop',
+            },
+          ],
+        },
+      }),
+    ],
+    base: '/root-digital-companion/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+  };
 });
